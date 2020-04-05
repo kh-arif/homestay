b0bf9d7b230f7384c2c1f15a148ca8aa927a1d17
khalid <khalid@pop-os.localdomain>
2020-04-04 21:43:48 -0700

    ss

--- inc/sendemail.php
+++ inc/sendemail.php
@@ -1,10 +1,10 @@
 <?php
 
-    $to = "backpiper.com@gmail.com";
+    $to = "khamuqbil@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $headers = "From: $from";
-    $subject = "You have a message from your BizPro";
+    $subject = "You have a message from your wibsite";
 
     $fields = array();
     $fields{"Fname"}    = "First Name";
@@ -12,7 +12,7 @@
     $fields{"email"}    = "Email";
     $fields{"sub"}    = "Subject";
     $fields{"message"}   = "Message";
-    
+
 
     $body = "Here is the message you got:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
 
